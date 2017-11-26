77b85b84ee213d4930368873e0f765db7d08b754
XavierDurbec <xavier-durbec@etud.univ-tln.fr>
2017-11-26 12:48:13 +0100

    rien d'interessant

--- js/webSql.js
+++ js/webSql.js
@@ -1,14 +1,10 @@
-var db;
-if(window.openDatabase){
-    db = openDatabase('ma_base', '1.0', 'database', 2000000, function(db){
-        db.changeVersion('', '1.0', function (t) {
-            t.executeSql('CREATE TABLE table1 (id, name)');
-        }, error);
+
+$(document).ready(function() {
+
+
+    $("form").insertBefore("<v>TA MERE</v>");
+    $( "#version" ).click(function() {
+
+        alert( "Handler for .click() called." );
     });
-
-
 }
-
-$("#button").click(function () {
-    alert(db.version);
-})
