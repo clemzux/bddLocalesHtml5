83d8ba08e5f11f76df72e752418f78c54d3e6eba
clement farge <farge.clement@gmail.com>
2017-11-26 17:25:41 +0100

    indexed fonctione avec chrome mais pas avec firefox, a suivre amélioration du code sur indexed

--- js/webStorage.js
+++ js/webStorage.js
@@ -14,42 +14,14 @@ function Entry() {
 
 $(document).ready(function() {
 
-    $("#valider").click(function () {
+    $("#persistWebStorage").click(function () {
 
-        var key = $("#mail").val();
-
-        var entry = new Entry();
-
-        entry.nom = $("#nom").val();
-        entry.prenom = $("#prenom").val();
-        entry.civilite = $("#civilite").val();
-        entry.rueVoie = $("#rueVoie").val();
-        entry.codePostal = $("#codePostal").val();
-        entry.ville = $("#ville").val();
-        entry.pays = $("#pays").val();
-        entry.numTel = $("#numTel").val();
-
-        put(key, JSON.stringify(entry));
+        persistEntry();
     });
 
     $("#affichageButton").click(function () {
 
-        var key = $("#key").val();
-        console.log(key)
-        var result = get(key);
-
-        var resultEntry = new Entry();
-
-        resultEntry = JSON.parse(result);
-
-        $("#nom").text(($("#nom").text() + resultEntry.nom));
-        $("#prenom").text(($("#prenom").text() + resultEntry.prenom));
-        $("#civilite").text(($("#civilite").text() + resultEntry.civilite));
-        $("#rueVoie").text(($("#rueVoie").text() + resultEntry.rueVoie));
-        $("#codePostal").text(($("#codePostal").text() + resultEntry.codePostal));
-        $("#ville").text(($("#ville").text() + resultEntry.ville));
-        $("#pays").text(($("#pays").text() + resultEntry.pays));
-        $("#numTel").text(($("#numTel").text() + resultEntry.numTel));
+        printEntry();
     });
 
     $("#delEntry").click(function () {
@@ -62,6 +34,44 @@ $(document).ready(function() {
         clearStorage();
     });
 });
+
+function printEntry() {
+
+    var key = $("#key").val();
+    console.log(key)
+    var result = get(key);
+
+    var resultEntry = new Entry();
+
+    resultEntry = JSON.parse(result);
+
+    $("#nom").text(($("#nom").text() + resultEntry.nom));
+    $("#prenom").text(($("#prenom").text() + resultEntry.prenom));
+    $("#civilite").text(($("#civilite").text() + resultEntry.civilite));
+    $("#rueVoie").text(($("#rueVoie").text() + resultEntry.rueVoie));
+    $("#codePostal").text(($("#codePostal").text() + resultEntry.codePostal));
+    $("#ville").text(($("#ville").text() + resultEntry.ville));
+    $("#pays").text(($("#pays").text() + resultEntry.pays));
+    $("#numTel").text(($("#numTel").text() + resultEntry.numTel));
+}
+
+function persistEntry() {
+
+    var key = $("#mail").val();
+
+    var entry = new Entry();
+
+    entry.nom = $("#nom").val();
+    entry.prenom = $("#prenom").val();
+    entry.civilite = $("#civilite").val();
+    entry.rueVoie = $("#rueVoie").val();
+    entry.codePostal = $("#codePostal").val();
+    entry.ville = $("#ville").val();
+    entry.pays = $("#pays").val();
+    entry.numTel = $("#numTel").val();
+
+    put(key, JSON.stringify(entry));
+}
 
 function put(key, data) {
 
