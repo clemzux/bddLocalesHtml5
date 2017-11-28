14b653c55c36a654598044acdfa2bb68f611dc09
XavierDurbec <xavier-durbec@etud.univ-tln.fr>
2017-11-28 09:35:25 +0100

    Exemple qui marche sur chrome

--- js/webSql.js
+++ js/webSql.js
@@ -1,40 +1,84 @@
-alert("JS lancer");
+
 
 $(document).ready(function() {
-    alert("Ready")
-})
 
-/*
-$("#ex").click(function () {
-    alert("lalala");
-    $("#ex").insertAfter("<div>Status Message</div>")
-})
-alert("St   a")
-    var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
 
-    var msg;
 
-    db.transaction(function (tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS LOGS (id unique, log)');
-        tx.executeSql('INSERT INTO LOGS (id, log) VALUES (1, "foobar")');
-        tx.executeSql('INSERT INTO LOGS (id, log) VALUES (2, "logmsg")');
-        msg = '<p>Log message created and row inserted.</p>';
-        document.querySelector('#status').innerHTML =  msg;
+
+
+if(window.openDatabase){
+    db = openDatabase('ma_base', '1.0', 'database', 2000000);
+
+    function displayResults( tx, results ){
+
+        if(results.rows.length == 0) {
+            alert("No records found");
+            return false;
+        }
+
+        var row = "";
+        for(var i=0; i<results.rows.length; i++) {
+            row += results.rows.item(i).rules + "<br/>";
+        }
+        //document.body.innerHTML = row
+        alert(row);
+    }
+
+    function onError(e){
+        alert(e.message);
+    }
+
+    function onSuccessExecuteSql( tx, results ){
+        alert('Execute SQL completed');
+    }
+
+    function onReadyTransaction( ){
+        alert( 'Transaction completed' );
+    }
+
+    db.transaction(
+
+        function(tx){
+            tx.executeSql(
+                "CREATE TABLE IF NOT EXISTS fightclub (id INTEGER PRIMARY KEY AUTOINCREMENT, rules TEXT)",
+                [],
+                onSuccessExecuteSql,
+                onError
+            )
+        },
+        onError,
+        onReadyTransaction
+    );
+
+    $("#ex").click(function(){
+
+        db.transaction(
+
+            function(tx){
+                tx.executeSql(
+                    'INSERT INTO fightclub (rules) VALUES (?)',
+                    ["lalalala"],
+                    onSuccessExecuteSql,
+                    onError
+                );
+            },
+            onError,
+            onReadyTransaction
+        );
     });
 
-    db.transaction(function (tx) {
-        tx.executeSql('SELECT * FROM LOGS', [], function (tx, results) {
-            var len = results.rows.length, i;
-            msg = "<p>Found rows: " + len + "</p>";
-            document.querySelector('#status').innerHTML +=  msg;
+} $("#sel").click(function(){
 
-            for (i = 0; i < len; i++){
-                msg = "<p><b>" + results.rows.item(i).log + "</b></p>";
-                document.querySelector('#status').innerHTML +=  msg;
-            }
-        }, null);
+        db.transaction(
+            function(tx){
+                tx.executeSql( "SELECT * FROM fightclub WHERE id > ?",
+                    ['1'],
+                    displayResults,
+                    onError )
+            },
+            onError,
+            onReadyTransaction
+        )
     });
 
-    alert("Version:"+ db.version);
-
-*/
+});
