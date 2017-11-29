2da4b422982caa0aa29d342bd213efcc57a195b4
XavierDurbec <xavier-durbec@etud.univ-tln.fr>
2017-11-29 20:06:38 +0100

    zpof

--- js/webSql.js
+++ js/webSql.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 
 if(window.openDatabase){
-    db = openDatabase('ma_base', '1.0', 'database', 2000000);
+    db = openDatabase('mon_carnet', '1.0', 'database', 2000000);
 
     function displayResults( tx, results ){
 
@@ -25,7 +25,7 @@ if(window.openDatabase){
     }
 
     function onError(e){
-        alert(e.message);
+        alert("Error 404");
     }
 
     function onSuccessExecuteSql( tx, results ){
@@ -37,10 +37,9 @@ if(window.openDatabase){
     }
 
     db.transaction(
-
         function(tx){
             tx.executeSql(
-                "CREATE TABLE IF NOT EXISTS personne (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, prenom TEXT, mail, civ  ENUM('Mr', 'Mme'),rueVoie Text, codePostal INTEGER, ville TEXT, pays TEXT, numTel INTEGER",
+                "CREATE TABLE IF NOT EXISTS personne (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, prenom TEXT, mail TEXT, civ  TEXT, rueVoie TEXT, codePostal INTEGER, ville TEXT, pays TEXT, numTel TEXT)",
                 [],
                 onSuccessExecuteSql,
                 onError
@@ -52,14 +51,25 @@ if(window.openDatabase){
 
 };
     
-    $("#persistWebSq").click(function () {
+    $("#persistWebSql").click(function () {
+        alert("Clic");
+        nom = $("#nom").val();
+        prenom = $("#prenom").val();
+        mail = $("#mail").val();
+
+
+        civilite = $('input[name=civilite]:checked').val()
+        rueVoie = $("#rueVoie").val();
+        codePostal = $("#codePostal").val();
+        ville = $("#ville").val();
+        pays= $("#pays").val();
+        numTel = $("#numTel").val();
 
         db.transaction(
-
             function(tx){
                 tx.executeSql(
-                    " INSERT INTO personne (nom,prenom,mail,civ,rueVoie,codePostal,ville,pays,numTel) VALUES (?,?,?,?,?,?,?,?,?)",
-                    [,,,,,,,,,],
+                    " INSERT INTO personne (nom,prenom,mail,civ) VALUES (?,?,?,?)",
+                    [nom,prenom,mail,civilite],//rueVoie,codePostal,ville,pays,numTel],
                     onSuccessExecuteSql,
                     onError
                 )
